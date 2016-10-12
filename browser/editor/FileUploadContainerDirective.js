8cb2ad694f6981444ec048d61a87c01ff4482143
wheresmycookie <robert@expii.com>
2016-10-11 20:57:40 -0400

    Fixed up the directives so that they're self-contained

--- browser/editor/FileUploadContainerDirective.js
+++ browser/editor/FileUploadContainerDirective.js
@@ -1,16 +1,17 @@
-    edMod.directive('dranFileUploadContainer', function () {
-        return {
-            restrict: 'AE',
-            link: function (scope, element) {
-                var input = element.find('input');
-                var button = element.find('button');
-                if (input.length && button.length) {
-                    button.bind('click', function (ev) {
-                        input[0].click();
-                    });
-                }
-                ;
-            }
-        };
-    });
+export default [FileUploadContainer];
 
+const FileUploadContainer = () => {
+  return {
+    restrict: 'E',
+    link: ($scope, $element) => {
+      const $input = $element.find('input');
+      const button = $element.find('button');
+
+      if ($input.length && button.length) {
+        button.bind('click', () => {
+          $input[0].click();
+        });
+      }
+    },
+  };
+}
--- browser/editor/StageContainerDirective.js
+++ browser/editor/StageContainerDirective.js
@@ -1,10 +1,11 @@
-    edMod.directive('dranStageContainer', ['model', function (model) {
-        return {
-            restrict: 'AE',
-            link: function (scope, element) {
-                /* element[0] gets the raw DOM reference from the jqlite object */
-                model.setup(element[0]);
-            }
-        };
-    }]);
+export default [StageContainer];
 
+const StageContainer = () => {
+  return {
+    restrict: 'AE',
+    link: function (scope, element) {
+      // FIXME: this is where they were loading model before, which is instantiated with new Dranimate();
+      model.setup(element[0]);
+    }
+  };
+};
