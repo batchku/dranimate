9d2525e79e2c20a1fe637370725b78bd9b42baa1
wheresmycookie <robert@expii.com>
2016-10-19 23:00:47 -0400

    Added in OpenCreatePuppetWindowDirective

---
+++ browser/editor/create_puppet_window/OpenCreatePuppetWindow.js
@@ -0,0 +1,40 @@
+var CreatePuppetWindowCtrl = require('./CreatePuppetWindowCtrl');
+
+module.exports = ['$mdMedia', '$mdDialog', 'imageToMesh', 'model', OpenCreatePuppetWindow];
+
+function makePuppetFromImageToMesh(imageToMesh, model) {
+  return function() {
+    imageToMesh.generateMesh();
+
+    var vertices = imageToMesh.getVertices();
+    var faces = imageToMesh.getTriangles();
+    var controlPoints = imageToMesh.getControlPointIndices();
+    var controlPointPositions = imageToMesh.getControlPoints();
+    var image = imageToMesh.getImage();
+    var imageNoBG = imageToMesh.getImageNoBackground();
+    var backgroundRemovalData = imageToMesh.getBackgroundRemovalData();
+
+    var p = new Puppet(image);
+    p.setImageToMeshData(imageNoBG, controlPointPositions, backgroundRemovalData);
+    p.generateMesh(vertices, faces, controlPoints);
+    model.addPuppet(p);
+  };
+};
+
+function OpenCreatePuppetWindow($mdMedia, $mdDialog, imageToMesh, model) {
+  return {
+    restrict: 'A',
+    link: function(scope, $element) {
+      $element.bind('click', function(e) {
+        $mdDialog.show({
+          controller: CreatePuppetWindowCtrl,
+          controllerAs: '$ctrl',
+          templateUrl: 'editor/create_puppet_window/create_puppet_window.html',
+          parent: angular.element(document.body),
+          closeTo: element,
+          fullscreen: $mdMedia('xs')
+        });
+      }).then(makePuppetFromImageToMesh(imageToMesh, model));
+    }
+  };
+}
--- browser/editor/create_puppet_window/index.js
+++ browser/editor/create_puppet_window/index.js
@@ -1,5 +1,5 @@
 var app = angular.module('dranimate.editor.createPuppetWindow', [
-  'ngMaterial',
+  'ngMaterial'
 ]);
 
 app.controller('CreatePuppetWindowCtrl', require('./CreatePuppetWindowCtrl'));
@@ -7,8 +7,4 @@ app.controller('CreatePuppetWindowCtrl', require('./CreatePuppetWindowCtrl'));
 app.directive('dranimateCancelEditPuppetDialog', require('./CancelEditPuppetDialog'));
 app.directive('dranimateFinishEditPuppetDialog', require('./FinishEditPuppetDialog'));
 app.directive('dranimateImageToMeshContainer', require('./ImageToMeshContainer'));
-
-app.component({
-  templateUrl: 'editor/create_puppet_window/create_puppet_window.html',
-  controller: 'EditPuppetDialogCtrl',
-});
+app.directive('dranimateOpenCreatePuppetWindow', require('./OpenCreatePuppetWindow'));
