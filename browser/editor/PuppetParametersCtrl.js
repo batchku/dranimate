06ec19965f925672eaa1bdde39d63e7d97662765
Nick Coronado <nicknado@Nicks-MBP.fios-router.home>
2016-10-25 23:16:11 -0400

    fixed X and Y

--- browser/editor/PuppetParametersCtrl.js
+++ browser/editor/PuppetParametersCtrl.js
@@ -3,14 +3,24 @@ module.exports = ['model', PuppetParametersCtrl];
 function PuppetParametersCtrl(model) {
   var $scope = this;
 
-  $scope.x = 0;
-  $scope.y = 0;
   $scope.rotation = 0;
+
+  $scope.x = function(value) {
+    var puppet = model.getSelectedPuppet();
+    if (!value) return (puppet && puppet['x']) || 0;
+    puppet['x'] = value;
+  };
+
+  $scope.y = function(value) {
+    var puppet = model.getSelectedPuppet();
+    if (!value) return (puppet && puppet['y']) || 0;
+    puppet['y'] = value;
+  };
 
   // TODO: merge scaleX and scaleY on the model
   $scope.scale = function(value) {
     var puppet = model.getSelectedPuppet();
-    if (!value) return puppet['scaleX'] * 100 || 100;
+    if (!value) return (puppet && puppet['scaleX'] * 100) || 100;
     puppet['scaleX'] = puppet['scaleY'] = value / 100;
   };
 
